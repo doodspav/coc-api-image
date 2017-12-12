0be8f1bb71b3bf9c6040ead3e03998f0eff5bafe
Tech <tsukuneesecc@gmail.com>
2017-12-12 14:41:02 -0700

    Update
    Updated

--- example.py
+++ example.py
@@ -1,11 +1,11 @@
-from main_profile import cocapi
-
+from main_profile import CoClient,cocapi
+client = CoClient("apiKey")
 
 player_hashtag = "#ULP92C2"
 file_name = "test" #default is "test"
 file_path = "" #default is "/var/www/html/"
 #having the file_path be "" saves the image in the same folder as main_profile.py
 
-player.makeProfile(player_hashtag, name=file_name, path=file_path)
+client.makeProfile(player_hashtag, name=file_name, path=file_path)
 
-print "Created a profile for %s (%s)." % (player.playerName, player.playerTag)
+print "Created a profile for %s (%s)." % (client.playerName, client.playerTag)
