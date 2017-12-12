ac3476736ac30cbae099dda60cb678183d4d456e
doodspav <doods9doods@gmail.com>
2017-12-12 08:15:41 +0000

    Create example.py

---
+++ example.py
@@ -0,0 +1,12 @@
+from main_profile import cocapi
+
+player = cocapi()
+
+player_hashtag = "#ULP92C2"
+file_name = "test" #default is "test"
+file_path = "" #default is "/var/www/html/"
+#having the file_path be "" saves the image in the same folder as main_profile.py
+
+player.makeProfile(player_hashtag, name=file_name, path=file_path)
+
+print "Created a profile for %s (%s)." % (player.playerName, player_hashtag)
