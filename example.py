e1f67ebff1e87a498efb0f0554386de6d9835033
doodspav <doods9doods@gmail.com>
2017-12-21 13:51:39 +0000

    added check response

--- example.py
+++ example.py
@@ -7,6 +7,8 @@ file_name = "test" #default is "test"
 file_path = "" #default is "/var/www/html/"
 #having the file_path be "" saves the image in the same folder as main_profile.py
 
-client.makeProfile(player_hashtag, name=file_name, path=file_path)
-
-print "Created a profile for %s (%s)." % (client.playerName, client.playerTag)
+response = client.makeProfile(player_hashtag, name=file_name, path=file_path)
+if response == 200:
+  print "Created a profile for %s (%s)." % (client.playerName, client.playerTag)
+else:
+  print response
