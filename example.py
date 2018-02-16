b3a8e765471d88a4b0b7ff4586b3b218a8262474
doodspav <doods9doods@gmail.com>
2018-02-16 12:05:11 +0000

    Update example.py

--- example.py
+++ example.py
@@ -1,13 +1,14 @@
-from main_profile import cocapi
+from profile_image import player
 
-client = cocapi()
+api_key = "Your api key. Check the README.md file to see how to get one."
+client = player(api_key)
 
-player_hashtag = "#ULP92C2"
+player_tag = "#ULP92C2"
 file_name = "test" #default is "test"
 file_path = "" #default is "/var/www/html/"
-#having the file_path be "" saves the image in the same folder as main_profile.py
+#having the file_path be "" saves the image in the same folder as profile_image.py
 
-response = client.makeProfile(player_hashtag, name=file_name, path=file_path)
+response = client.main_full_profile(player_tag, name=file_name, path=file_path)
 if response == 200:
   print "Created a profile for %s (%s)." % (client.playerName, client.playerTag)
 else:
