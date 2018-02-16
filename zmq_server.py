51fe69c5d02834c6cf4c1985f2ff208678695701
doodspav <doods9doods@gmail.com>
2018-02-16 12:14:54 +0000

    Update zmq_server.py

--- zmq_server.py
+++ zmq_server.py
@@ -10,10 +10,10 @@ api_key = "api_key"
 client = player(api_key)
 
 while True:
-	player_tag = socket.recv()
-	file_name = str(random.randint(10000,99999))
-	response = client.main_full_profile(player_tag, name=file_name)
-	if response == 200:
-		socket.send(b"%s.jpg" % file_name)
-	else:
-		socket.send(b"Error: %s" % client.status_reasons[response])
+    player_tag = socket.recv()
+    file_name = str(random.randint(10000,99999))
+    response = client.main_full_profile(player_tag, name=file_name)
+    if response == 200:
+        socket.send(b"%s.jpg" % file_name)
+    else:
+        socket.send(b"Error: %s" % client.status_reasons[response])
